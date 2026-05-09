536b8d7bad7a737dc9691de657f92431b53d343e
ApothecaryMan <mohamed1232003@gmail.com>
2026-05-09 22:34:25 +0300

    chore: upgrade tauri-plugin-process to v2.3.1 and register it in the application builder

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,6 +9,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_thermal_printer::init())
         .setup(|app| {
             // Debug-only logging
