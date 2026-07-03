3a9011b995f3538c9b58c7237295e50c2936ab97
mohamed_rezk_win <mohamed1232003@gmail.com>
2026-07-03 19:17:05 +0300

    update

--- public/preflight.js
+++ public/preflight.js
@@ -3,7 +3,7 @@
  * Keeps lightweight boot-time version, theme, and direction setup.
  */
 (() => {
-  const CURRENT_VERSION = '2.062';
+  const CURRENT_VERSION = '2.063';
   const VERSION_KEY = 'pharma_storage_version';
   const SESSION_KEY = 'branch_pilot_session';
 
